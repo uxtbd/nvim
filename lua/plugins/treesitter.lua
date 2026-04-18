170aca4a122e0c291f43c8b06637925708f22544
uxtbd <dev@kthread.dev>
2026-04-18 15:21:22 -0300

    more treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -27,6 +27,8 @@ return {
 				"css",
 				"scss",
 				"python",
+				"regex",
+				"query",
 			}, -- list of parsers to install at the start of a neovim session
 			border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
 			auto_install = true, -- if enabled, install missing parsers when editing a new file
